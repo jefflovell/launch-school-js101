4f6e7f965187b12c818ab835b128cc5c58d82b94
Jeff Lovell <jeff@lovellworks.com>
2021-08-06 18:49:07 -0400

    calculator bonus questions

--- lesson2/calculator.js
+++ lesson2/calculator.js
@@ -5,6 +5,7 @@
 // Print the results to the terminal.
 
 const readline = require('readline-sync');
+const messages = require('./calculator_messages.json')
 
 function prompt(message) {
   console.log(`=> ${message}`);
@@ -15,49 +16,94 @@ function invalidNumber(number) {
 }
 
 //init
-prompt('Welcome to Calculator!');
+prompt(messages.lang);
+let lang = readline.question();
 
-//ask user for first number and store input
-prompt("Please input your first number");
-
-let number1 = readline.question();
-while (invalidNumber(number1)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number1 = readline.question();
+while (!['1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(lang)) {
+  prompt('Please choose a valid language');
+  lang = readline.question();
 }
 
-//ask user for second number and store input
-prompt("Please input your second number");
-
-let number2 = readline.question();
-while (invalidNumber(number2)) {
-  prompt("Hmm... that doesn't look like a valid number.");
-  number2 = readline.question();
-}
-
-//ask user for operation to perform and store input
-prompt("What operation would you like to perform?\n1 => Addition\n2 => Subtraction\n3 => Multiplication\n4 => Division");
-let operation = readline.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Hmm... Please choose 1, 2, 3 or 4');
-  operation = readline.question();
-}
-
-let output;
-switch (operation) {
+switch (lang) {
   case '1':
-    output = Number(number1) + Number(number2);
+    lang = "en";
     break;
   case '2':
-    output = Number(number1) - Number(number2);
+    lang = "es";
     break;
   case '3':
-    output = Number(number1) * Number(number2);
+    lang = "fr";
     break;
   case '4':
-    output = Number(number1) / Number(number2);
+    lang = "de";
+    break;
+  case '5':
+    lang = "sr";
+    break;
+  case '6':
+    lang = "ru";
+    break;
+  case '7':
+    lang = "ca";
+    break;
+  case '8':
+    lang = "jp";
+    break;
+  case '9':
+    lang = "jp";
     break;
 }
 
-prompt(`The result is: ${output}`);
+prompt(messages[lang].welcome);
+
+//ask user for first number and store input
+while (true) {
+
+  prompt(messages[lang].num1);
+
+  let number1 = readline.question();
+  while (invalidNumber(number1)) {
+    prompt(messages[lang].invalid);
+    number1 = readline.question();
+  }
+
+  //ask user for second number and store input
+  prompt(messages[lang].num2);
+
+  let number2 = readline.question();
+  while (invalidNumber(number2)) {
+    prompt(messages[lang].invalid);
+    number2 = readline.question();
+  }
+
+  //ask user for operation to perform and store input
+  prompt(messages[lang].operation);
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Hmm... Please choose 1, 2, 3 or 4');
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(messages[lang].result + output);
+
+  prompt(messages[lang].repeat);
+  let repeat = readline.question();
+  if (repeat !== 1) break;
+}
