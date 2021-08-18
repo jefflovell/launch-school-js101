0ce0de15f35d3cfcd3e4ab11bf94ac9ff6711a1e
Jeff Lovell <jeff@lovellworks.com>
2021-08-18 14:38:08 -0400

    bonus questions 1 & 2
    added 'lizard' and 'spock' to VALID_CHOICES
    allowed for single letter input from users

--- lesson2/rock_paper_scissors.js
+++ lesson2/rock_paper_scissors.js
@@ -1,20 +1,27 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const DISPLAY_CHOICES = ['[r]ock', '[p]aper', '[s]cissors', '[l]izard', 'sp[o]ck'];
+const VALID_CHOICES_LONG = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const VALID_CHOICES_SHORT = ['r', 'p', 's', 'l', 'o'];
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+// eslint-disable-next-line complexity
 function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-    (choice === 'paper' && computerChoice === 'rock') ||
-    (choice === 'scissors' && computerChoice === 'paper')) {
+  if ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
+    (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
+    (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
+    (choice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
+    (choice === 'spock' && (computerChoice === 'rock' || computerChoice === 'scissors'))) {
     prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-            (choice === 'paper' && computerChoice === 'scissors') ||
-            (choice === 'scissors' && computerChoice === 'rock')) {
+  } else if ((choice === 'rock' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
+            (choice === 'paper' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
+            (choice === 'scissors' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
+            (choice === 'lizard' && (computerChoice === 'scissors' || computerChoice === 'rock')) ||
+            (choice === 'spock' && (computerChoice === 'paper' || computerChoice === 'lizard'))) {
     prompt('Computer wins!');
   } else {
     prompt("It's a tie!");
@@ -30,16 +37,34 @@ function playAgain(answer) {
 }
 
 while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt(`Choose one by typing the word or the letter in [brackets]:\n=>${DISPLAY_CHOICES.join(', ')}`);
   let choice = readline.question().toLowerCase();
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!(VALID_CHOICES_LONG.includes(choice) || VALID_CHOICES_SHORT.includes(choice))) {
     prompt("That's not a valid choice");
     choice = readline.question();
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  switch (choice) {
+    case 'r':
+      choice = 'rock';
+      break;
+    case 'p':
+      choice = 'paper';
+      break;
+    case 's':
+      choice = 'scissors';
+      break;
+    case 'l':
+      choice = 'lizard';
+      break;
+    case 'o':
+      choice = 'spock';
+      break;
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES_LONG.length);
+  let computerChoice = VALID_CHOICES_LONG[randomIndex];
 
   displayWinner(choice, computerChoice);
 
