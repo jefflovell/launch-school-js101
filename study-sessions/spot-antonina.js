d8a444c7b023e01d79a5a80959a4ef74c7700356
Jeff Lovell <jeff@lovellworks.com>
2021-09-03 10:55:50 -0400

    update spot-antonina.js

--- study-sessions/spot-antonina.js
+++ study-sessions/spot-antonina.js
@@ -1,5 +1,30 @@
 // Aug 31, 2021 - Antonina
 
+// 109 Study Guide
+  -declarations, initialization, assignment, and re-assignment
+  -variable scope, especially how variables interact with function definitions and blocks
+  -primitive values, objects, and type coercions
+  -object properties
+  -mutability vs. immutability vs. const
+  -loose and strict equality
+  -passing arguments into and return values out of functions
+  -working with Strings
+  -working with Arrays, especially the iteration methods (forEach, map, filter, and find)
+  -working with Objects; accessing keys and values of an Object as arrays
+  -arrays are objects
+  -understand the concepts of pass-by-reference and pass-by-value
+variables as pointers
+console.log vs. return
+truthiness vs. boolean
+function definition and invocation
+function declarations, function expressions, and arrow functions
+implicit return value of function invocations
+first-class functions
+side-effects
+naming conventions (legal vs idiomatic)
+be able to explain what a function does without talking about its implementation; that is, document a function's use and purpose.
+
+
 // Question 15 - Kris
 
 let family = {
