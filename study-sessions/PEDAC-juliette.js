3135978646193dc08fb01368f62ec6378e882c90
Jeff Lovell <jeff@lovellworks.com>
2021-09-21 11:57:34 -0400

    Create PEDAC session julliete

---
+++ study-sessions/PEDAC-juliette.js
@@ -0,0 +1,63 @@
+/*
+Solve the Problem on the logical level first.
+
+=========================
+Understanding the Problem
+=========================
+
+Goal: understand what you're being asked to do.
+
+Establish the rules & boundaries of the problem?
+What are the implicit and explicit requirements?
+Ask clarifying questions.
+Make note of any questions you hope to answer by examining the test cases in the next step.
+
+rules:
+- the output needs to be sorted in x order
+- input will always be positive number
+
+=======================
+Examples and test cases
+=======================
+
+Goal: understand more about what you're being asked to do.
+
+We should use our examination of the test cases to confirm or refute our assumptions of the problem.
+A good way to see the rules/boundaries in actions.
+If the return value is an array-- take a look at the order of elements.
+If problem involves strings-- should we treat strings in a case-insensitive manner?
+What about empty arguments?
+Walk through all test cases with your understanding of the problem and see if they confirm your understanding.
+May need/want to write some of your own test cases.
+
+================
+Data structures
+================
+
+Goal: begin to think logically and solution-based about the problem.
+
+How we structure our data is closely related to our algorithm and the steps we may take to solve the problem.
+Helps us to reason with our data more logically.
+What does our data look like when we get it? (input)
+What does our data look like when we return it? (output)
+What does our data need to look like in between (intermediary steps)?
+
+================
+Algorithm
+================
+
+Goal: write out high-level steps to solve the given problem.
+
+A logical sequence of steps for accomplishing a task or objective.
+Be abstract and high-level (don't box yourself in to a specific implementation, for example).
+Language-agnostic.  Your algorithm should be applicable no matter what language someone is using to code a solution.
+Find a solution that works (don't worry about efficiency for now)
+Can create substeps for different parts
+Separate concerns can potentially be moved into a helper function.
+You can (and should) return to your algorithm during the implementation stage to update.
+Although you want to keep your algorithm high-level in that you don't want your algorithm to be code-specific, you should take care not to skip over necessary steps in the problem solving process.  For example, if a step in your algorithm is 'determine if number is prime'.  There should be sub-steps there walking through how you would determine if the number is prime.
+
+==================
+Coding with intent
+==================
+*/
